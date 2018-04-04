da9ba40dd60037f50f1d71692970be0c34f5eacc
shingodesigns <info@shingodesigns.com>
2018-04-04 23:32:05 +1200

    Update

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', function(event) {
 console.log(event.request.url);
 event.respondWith(
 caches.match(event.request).then(function(response) {
-return response || fetch(event.request);
+return response || fetch(event.request, {mode:'no-cors'});
 })
 );
 });
