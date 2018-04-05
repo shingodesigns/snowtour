c80ffd79bf632981592ef607f394f3a4093ab2f7
shingodesigns <info@shingodesigns.com>
2018-04-05 23:07:21 +1200

    Fix

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,4 +1,4 @@
-(function(){function a(a,c){var b=Math.floor(((new Date(c)).getTime()-(new Date).getTime())/864E5);0>b&&(b=0);document.getElementById(a).innerHTML=b+" days to go!"}
+(function(){function a(a,c){var b=Math.ceil(((new Date(c)).getTime()-(new Date).getTime())/864E5);0>b&&(b=0);document.getElementById(a).innerHTML=b+" days to go!"}
 a("cd0","2018-06-02");
 a("cd1","2018-06-16");
 a("cd2","2018-06-23");
