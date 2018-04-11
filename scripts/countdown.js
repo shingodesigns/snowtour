66b41ae52d07971d316b6f4debcea35cce35ff16
shingodesigns <info@shingodesigns.com>
2018-04-11 23:25:49 +1200

    Update

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,6 +1,6 @@
-(function(){function a(a,c){var b=Math.ceil(((new Date(c)).getTime()-(new Date).getTime())/864E5);0>b&&(b=0);document.getElementById(a).innerHTML=b+" days to go!"}
-a("cd0","2018-06-02");
-a("cd1","2018-06-16");
-a("cd2","2018-06-29");
-a("cd3","2018-06-30");
+(function(){function a(a,b,c,d){b=Math.ceil(((new Date(b,c-1,d)).getTime()-(new Date).getTime())/864E5);0>b&&(b=0);document.getElementById(a).innerHTML=b+" days to go!"}
+a("cd0",2018,6,2);
+a("cd1",2018,6,16);
+a("cd2",2018,6,29);
+a("cd3",2018,6,30);
 })();
