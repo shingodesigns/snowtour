53c694993b3f598faadb77cfe4b6d6d163fe5f33
shingodesigns <info@shingodesigns.com>
2018-07-11 23:29:28 +1200

    Update

--- scripts/status.js
+++ scripts/status.js
@@ -1,1 +1,1 @@
-angular.module("a",[]).controller("c",["$http",function(a){var b=this;b.c=function(){a.get(b.s+".json").then(function(a){b.d=a.data})}}]);
+angular.module("a", []).controller("c",["$http",function(c){var b=this;b.c=function(){c.get(b.a+".json").then(function(a){b.d=a.data})};b.s=function(a){return"OPEN"==a?"green":"CLOSED"==a?"red":"orange"};b.w=function(a){return 20>a?"white":40>a?"green":60>a?"yellow":80>a?"orange":"red"}}]);
