272cf682d53314db383e210751296a8175a94960
shingoaoyama1 <shingo.aoyama1@gmail.com>
2019-05-26 22:36:47 +1200

    Update

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,6 +1,5 @@
 (function(){function a(a,b,c,d){b=Math.ceil(((new Date(b,c-1,d)).getTime()-(new Date).getTime())/864E5);0>b&&(b=0);document.getElementById(a).innerHTML=b+" days to go!"}
-a("cd0",2018,6,2);
-a("cd1",2018,6,16);
-a("cd2",2018,6,29);
-a("cd3",2018,6,30);
+a("cd0",2019,6,1);
+a("cd1",2019,6,22);
+a("cd2",2019,6,29);
 })();
