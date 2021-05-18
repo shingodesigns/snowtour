4bb3488d4c3be511f4d875dead9586e1e501cb0e
shingoaoyama1 <shingo.aoyama1@gmail.com>
2021-05-18 21:12:28 +1200

    Update countdown

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,5 +1,5 @@
 (function(){function a(a,b,c,d){b=Math.ceil(((new Date(b,c-1,d)).getTime()-(new Date).getTime())/864E5);0>b&&(b=0);document.getElementById(a).innerHTML=b+" days to go!"}
-a("cd0",2019,6,1);
-a("cd1",2019,6,22);
-a("cd2",2019,6,29);
+a("cd0",2021,6,5);
+a("cd1",2021,7,3);
+a("cd2",2021,7,3);
 })();
